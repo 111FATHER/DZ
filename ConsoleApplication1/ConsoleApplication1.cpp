d86621aead3b40412fcbce04d26b5ba8acd23b45
111FATHER <serezarabisev@gmail.com>
2025-10-24 00:25:58 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <Windows.h>
+#include <iomanip>
+
+
+
+int main()
+{
+
+	//setlocale(LC_ALL, "ru");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	srand(time(NULL));
+
+	
+
+
+return 0;
+
+
+}
