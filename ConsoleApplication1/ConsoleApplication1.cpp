396318680014f890ab38b2bedcfdd2029bb72ece
111FATHER <serezarabisev@gmail.com>
2025-11-03 21:58:54 +0500

    18.10.25

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -83,63 +83,173 @@
 //	std::cout << "Максимальное число - " << max << "\n";
 //}
 
-void Randarr()
+//void Randarr()
+//{
+//	int arr[10];
+//	for (int i = 0; i < 10; i++)
+//	{
+//		arr[i] = rand() % 100 + 1;
+//		std::cout << arr[i] << " ";
+//	}
+//	std::cout << "\n";
+//
+//	int min = arr[0];
+//	int max = arr[0];
+//
+//	for (int i = 0; i < 10; i++)
+//	{
+//		if (arr[i] < min)
+//		{
+//			min = arr[i];
+//		}
+//		if (arr[i] > max)
+//		{
+//			max = arr[i];
+//		}
+//	}
+//	std::cout << "маленькое - " << min << "\n";
+//	std::cout << "большое - " << max << "\n";
+//}
+//void Randarr2()
+//{
+//	int minRange, maxRange;
+//	std::cout << "Введите минимальное значение диапазона - ";
+//	std::cin >> minRange;
+//	std::cout << "Введите максимальное значение диапазона - ";
+//	std::cin >> maxRange;
+//
+//	int arr[10];
+//	std::cout << "Массив - ";
+//	for (int i = 0; i < 10; i++) {
+//		arr[i] = rand() % (maxRange - minRange + 1);
+//		std::cout << arr[i] << " ";
+//	}
+//	std::cout << "\n";
+//
+//	int value;
+//	std::cout << "Введите значение для сравнения - ";
+//	std::cin >> value;
+//
+//	int sum = 0;
+//	for (int i = 0; i < 10; i++) {
+//		if (arr[i] < value) {
+//			sum += arr[i];
+//		}
+//	}
+//	std::cout << "Сумма элементов, меньших " << value << " - " << sum;
+//	std::cout << "\n";
+//}
+
+void year(int year) 
 {
-	int arr[10];
-	for (int i = 0; i < 10; i++)
-	{
-		arr[i] = rand() % 100 + 1;
-		std::cout << arr[i] << " ";
-	}
-	std::cout << "\n";
-
-	int min = arr[0];
-	int max = arr[0];
-
-	for (int i = 0; i < 10; i++)
-	{
-		if (arr[i] < min)
-		{
-			min = arr[i];
-		}
-		if (arr[i] > max)
-		{
-			max = arr[i];
-		}
-	}
-	std::cout << "маленькое - " << min << "\n";
-	std::cout << "большое - " << max << "\n";
+    if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) 
+    {
+        std::cout << "Год " << year << " високосный\n";
+    }
+    else 
+    {
+        std::cout << "Год " << year << " не високосный\n";
+    }
 }
-void Randarr2()
+void days(int year1, int month1, int day1, int year2, int month2, int day2) 
 {
-	int minRange, maxRange;
-	std::cout << "Введите минимальное значение диапазона - ";
-	std::cin >> minRange;
-	std::cout << "Введите максимальное значение диапазона - ";
-	std::cin >> maxRange;
-
-	int arr[10];
-	std::cout << "Массив - ";
-	for (int i = 0; i < 10; i++) {
-		arr[i] = rand() % (maxRange - minRange + 1);
-		std::cout << arr[i] << " ";
-	}
-	std::cout << "\n";
-
-	int value;
-	std::cout << "Введите значение для сравнения - ";
-	std::cin >> value;
-
-	int sum = 0;
-	for (int i = 0; i < 10; i++) {
-		if (arr[i] < value) {
-			sum += arr[i];
-		}
-	}
-	std::cout << "Сумма элементов, меньших " << value << " - " << sum;
-	std::cout << "\n";
+    int daysInMonth[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    int totalDays1 = 0;
+    for (int i = 1; i < year1; i++) 
+    {
+        if ((i % 4 == 0 && i % 100 != 0) || (i % 400 == 0)) 
+        {
+            totalDays1 += 366;
+        }
+        else 
+        {
+            totalDays1 += 365;
+        }
+    }
+    for (int i = 1; i < month1; i++) 
+    {
+        if (i == 2 && ((year1 % 4 == 0 && year1 % 100 != 0) || (year1 % 400 == 0))) 
+        {
+            totalDays1 += 29;
+        }
+        else 
+        {
+            totalDays1 += daysInMonth[i - 1];
+        }
+    }
+    totalDays1 += day1;
+    int totalDays2 = 0;
+    for (int i = 1; i < year2; i++)
+    {
+        if ((i % 4 == 0 && i % 100 != 0) || (i % 400 == 0))
+        {
+            totalDays2 += 366;
+        }
+        else 
+        {
+            totalDays2 += 365;
+        }
+    }
+    for (int i = 1; i < month2; i++)
+    {
+        if (i == 2 && ((year2 % 4 == 0 && year2 % 100 != 0) || (year2 % 400 == 0)))
+        {
+            totalDays2 += 29;
+        }
+        else 
+        {
+            totalDays2 += daysInMonth[i - 1];
+        }
+    }
+    totalDays2 += day2;
+    int difference;
+    if (totalDays2 > totalDays1) 
+    {
+        difference = totalDays2 - totalDays1;
+    }
+    else 
+    {
+        difference = totalDays1 - totalDays2;
+    }
+    std::cout << "Разница между " << day1 << "." << month1 << "." << year1 << " и " << day2 << "." << month2 << "." << year2 << " составляет " << difference << " дней\n";
 }
-\
+void average(int arr[], int size) 
+{
+    int sum = 0;
+    for (int i = 0; i < size; i++) 
+    {
+        sum += arr[i];
+    }
+    double average = 1.0 * sum / size;
+    std::cout << "Среднее арифметическое - " << average;
+    std::cout << "\n";
+}
+void elements(int arr[], int size) 
+{
+    int plus = 0, minus = 0, nol = 0;
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] > 0) {
+            plus++;
+        }
+        else if (arr[i] < 0)
+        {
+            minus++;
+        }
+        else
+        {
+            nol++;
+        }
+    }
+
+    std::cout << "Положительных элементов: " << plus;
+    std::cout << "\n";
+    std::cout << "Отрицательных элементов: " << minus;
+    std::cout << "\n";
+    std::cout << "Нулевых элементов: " << nol;
+    std::cout << "\n";
+}
+
 
 int main()
 {
@@ -159,9 +269,7 @@ int main()
 	std::cout << "ЗАДАНИЕ 3\n";
 	int num33 = 0;
 	numbers3(num33);*/
-
-
-	std::cout << "ЗАДАНИЕ 1\n";
+/*std::cout << "ЗАДАНИЕ 1\n";
 	Randarr();
 	std::cout << "\n";
 
@@ -170,7 +278,71 @@ int main()
 	std::cout << "\n";
 
 	std::cout << "ЗАДАНИЕ 3\n";
-	std::cout << "Не понимаю как сделать, даже с gpt не понимаю строчки";
+	std::cout << "Не понимаю как сделать, даже с gpt не понимаю строчки";*/
+
+    std::cout << "ЗАДАНИЕ 1\n";
+    int year1, month1, day1, year2, month2, day2;
+
+    std::cout << "Введите первую дату\n";
+    std::cout << "Год - ";
+    std::cin >> year1;
+    std::cout << "Месяц - ";
+    std::cin >> month1;
+    std::cout << "День - ";
+    std::cin >> day1;
+
+    std::cout << "\nВведите вторую дату\n";
+    std::cout << "Год - ";
+    std::cin >> year2;
+    std::cout << "Месяц - ";
+    std::cin >> month2;
+    std::cout << "День - ";
+    std::cin >> day2;
+
+    std::cout << "\n Високосный год \n";
+    year(year1);
+    year(year2);
+
+    std::cout << "\n Расчет разницы в днях \n";
+    days(year1, month1, day1, year2, month2, day2);
+
+    std::cout << "\nЗАДАНИЕ 2\n";
+    int u;
+    std::cout << "Введите размер массива - ";
+    std::cin >> u;
+    int* arr = new int[u];
+    std::cout << "Введите элементы массива\n";
+    for (int i = 0; i < u; i++)
+    {
+        std::cout << "Элемент " << i + 1 << " - ";
+        std::cin >> arr[i];
+    }
+    std::cout << "\nРезультат\n";
+    average(arr, u);
+    delete[] arr;
+
+    std::cout << "\nЗАДАНИЕ 3\n";
+    int y;
+    std::cout << "Введите размер массива - ";
+    std::cin >> y;
+
+    int* arr1 = new int[y];
+
+    std::cout << "Введите элементы массива";
+    std::cout << "\n";
+    for (int i = 0; i < y; i++)
+    {
+        std::cout << "Элемент " << i + 1 << " - ";
+        std::cin >> arr1[i];
+    }
+
+    std::cout << "\nРезультат";
+    std::cout << "\n";
+    elements(arr1, y);
+
+    delete[] arr1;
+
+
 
 
 
